685b31b5fff6b04e98c8e472897832d4ace75b23
Elder Lucas <elderlucas@gmail.com>
2015-11-23 17:21:31 -0200

    Restaurant Menu Dev

--- project.py
+++ project.py
@@ -15,8 +15,7 @@ import requests
 app = Flask(__name__)
 
 
-CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('client_secrets.json','r').read())['web']['client_id']
 APPLICATION_NAME = "Restaurant Menu Application"
 
 # Create anti-forgery state token
