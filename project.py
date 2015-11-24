90cb86828ff9017681a6cd43eb91bfa7e026326c
Elder Lucas <elderlucas@gmail.com>
2015-11-24 11:32:55 -0200

    Adicionado a visualizacao dos menu items

--- project.py
+++ project.py
@@ -28,12 +28,22 @@ app_path = os.getcwd()
 os.chdir(app_path)
 
 
+####################### Carregando o Arquivo JSON ########################
+# Eh importante lembrar que esse if serve para verificar se o arquivo 
+# existe num current diretorio, o que se caso nao existir, o apache
+# que esta rodando do "sever side" ira busca-lo em um lugar especifico
+##########################################################################
 if ((os.path.isfile('client_secrets.json'))== True):
 	opened_file = open('client_secrets.json', 'r')
+	print "Ok, Im in Local Side"
 else:
 	opened_file = open('/var/www/restaurant_py/client_secrets.json', 'r')
+	print "Ohhh, Im in a Server Side"
 
 CLIENT_ID = json.loads(opened_file.read())['web']['client_id']
+
+
+
 
 APPLICATION_NAME = "Restaurant Menu Application"
 
@@ -44,7 +54,6 @@ def showLogin():
                     for x in xrange(32))
 
     STATE = state
-
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     print "Login"
@@ -126,18 +135,24 @@ def deleteRestaurant(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
 
-    if DEBUG_ALL is True: 
-        print "################################################################"
-        print "SHOWING ALL MENU ITEMS"
-
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     creator = getUserInfo(restaurant.user_id)
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
 
-    if 'email' not in login_session:
-    	current_user_id = getUserID(login_session['email'])
+    if 'username' not in login_session:
+        print "Public restaurant/...... User Doesn't Exist"
+        return render_template('publicmenu.html', items=items, restaurant=restaurant, creator=creator)
     else:
-    	return "Restaurant Menu Error"
+    	if 'email' in login_session:
+    		print "Verify Email"
+    		current_user_id = getUserID(login_session['email'])
+    		if creator.id == current_user_id:
+    			return render_template('menu.html', items=items, restaurant=restaurant, creator=creator)
+    		else:
+    			return render_template('publicmenu.html', items=items, restaurant=restaurant, creator=creator)
+    	else:
+    		print "Email Error... Is not the Same User"
+    		return render_template('publicmenu.html', items=items, restaurant=restaurant, creator=creator)
 
     if DEBUG_ALL is True: 
         print "Restaurant: ---->"
@@ -148,22 +163,6 @@ def showMenu(restaurant_id):
         print creator.id
         print creator.name
         print creator.email
-        print "################################################################"
-
-    if 'username' not in login_session or creator.id != current_user_id:
-        print "Public restaurant/"
-        return render_template('publicmenu.html', items=items, restaurant=restaurant, creator=creator)
-    else:
-        print "Ok, im a owner of this menu item"     
-        if DEBUG_ALL is True: 
-            print "################################################################"
-            print "My Current User id...."
-            print current_user_id
-            print "Email of User Owner of this :"
-            print login_session['email']
-            print "################################################################"
-
-        return render_template('menu.html', items=items, restaurant=restaurant, creator=creator)
 
 
 # Create a new menu item
